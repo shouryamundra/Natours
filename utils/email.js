c75f9d031ea7e1a315d520d0e805bb2f690821d2
shouryamundra <shouryamern@gmail.com>
2020-11-04 10:22:25 +0000

    Natours API Updates (Bugs)

--- utils/email.js
+++ utils/email.js
@@ -13,7 +13,7 @@ const sendEmail = async (options) => {
 
   // 2) Define the email options
   const mailOptions = {
-    from: 'Shourya Mundra <natours@shourya.io>',
+    from: 'Admin-Natours <admin@natours.io>',
     to: options.email,
     subject: options.subject,
     text: options.message,
